476786b7e5ba559dfbe455603c532a387392ee06
Nader Nour <nnmh1992@hotmail.com>
2017-02-24 20:11:52 +0300

    Alot of Edits, finished intial system in the puzzle game

--- Assets/Scripts/PuzzleGenerator.cs
+++ Assets/Scripts/PuzzleGenerator.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PuzzleGenerator : MonoBehaviour {
+public class PuzzleGenerator : MonoBehaviour
+{
+    public int NoOfPieces = 35;
+    public RectTransform ParentCanvas;
     public RectTransform[] RandomPositions;
+    public RectTransform DragingLimitRect;
+    public RectTransform SelectedPieceParent;
     public float RandomRadius;
+    public static PuzzleGenerator singleton;
 
 
+    private int SolvedPiecesCounter = 0 ;
     PuzzlePiece[] PuzzlePieces;
-	// Use this for initialization
-	void Start () {
-        PuzzlePieces = GetComponentsInChildren<PuzzlePiece>();
-		foreach(var Piece in PuzzlePieces)
-        {
-            Piece.SetRandomPositionAndRotation(RandomPositions[Random.Range(0, RandomPositions.Length)].position, RandomRadius);
-        }
-	}
-	
-	// Update is called once per frame
-	void Update ()
+
+    private void Awake()
     {
-		
-	}
+        if(!singleton)
+        {
+            singleton = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector2 MinDraging = (Vector2)DragingLimitRect.position + DragingLimitRect.rect.min * ParentCanvas.localScale.x;
+        Vector2 MaxDraging = (Vector2)DragingLimitRect.position + DragingLimitRect.rect.max * ParentCanvas.localScale.x;
+        PuzzlePieces = GetComponentsInChildren<PuzzlePiece>();
+        foreach (var Piece in PuzzlePieces)
+        {
+            Piece.RandomizeThePiece(RandomPositions[Random.Range(0, RandomPositions.Length)].position, RandomRadius, MinDraging, MaxDraging, SelectedPieceParent);
+        }
+    }
+
+    public void OnPieceSolved()
+    {
+        SolvedPiecesCounter++;
+    }
 }
--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -1,22 +1,27 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
-
-public class PuzzlePiece : EventTrigger {
+using UnityEngine.UI;
+public class PuzzlePiece : EventTrigger
+{
     private Vector3 EndPosition;
     private float StartDistance;
     private float StartRotation;
     private RectTransform m_Transform;
-    // Use this for initialization
+    private Vector2 MinDragingLimits;
+    private Vector2 MaxDragingLimits;
+    private RectTransform SelectedPieceParent;
+    private Transform NormalParent;
+    private bool bSolved;
 
-    private void Awake()
+    public void RandomizeThePiece(Vector2 InPos, float CircleRed, Vector2 _MinDragingLimits, Vector2 _MaxDraginLimits, RectTransform _SelectedPieceParent)
     {
+        NormalParent = transform.parent;
+        SelectedPieceParent = _SelectedPieceParent;
+        MinDragingLimits = _MinDragingLimits;
+        MaxDragingLimits = _MaxDraginLimits;
         m_Transform = GetComponent<RectTransform>();
-        EndPosition = transform.position;
-    }
-
-    public void SetRandomPositionAndRotation(Vector2 InPos, float CircleRed)
-    {
-         m_Transform.position = InPos + Random.insideUnitCircle * CircleRed;
+        EndPosition = m_Transform.position;
+        m_Transform.position = InPos + Random.insideUnitCircle * CircleRed;
         StartDistance = Vector3.Distance(EndPosition, m_Transform.position);
         StartRotation = Random.value;
         Vector3 TempRotation = new Vector3(0, 0, StartRotation * 360);
@@ -25,12 +30,37 @@ public class PuzzlePiece : EventTrigger {
 
     public override void OnDrag(PointerEventData eventData)
     {
+        if (bSolved)
+            return;
         base.OnDrag(eventData);
-        m_Transform.position = Input.mousePosition;
+        Vector3 MousePos = Input.mousePosition;
+        MousePos.x = Mathf.Clamp(MousePos.x, MinDragingLimits.x , MaxDragingLimits.x);
+        MousePos.y = Mathf.Clamp(MousePos.y, MinDragingLimits.y, MaxDragingLimits.y);
+        m_Transform.position = MousePos;
         float CurrentDistance = Vector3.Distance(EndPosition, m_Transform.position);
-        float value  = Mathf.InverseLerp(StartDistance, 0, CurrentDistance);
-        value = ;
+        if(CurrentDistance < 30)
+        {
+            m_Transform.position = EndPosition;
+            m_Transform.rotation = Quaternion.identity;
+            bSolved = true;
+            PuzzleGenerator.singleton.OnPieceSolved();
+            return;
+        }
+        float value = Mathf.InverseLerp(StartDistance, 0, CurrentDistance);
         Vector3 TempRotation = new Vector3(0, 0, Mathf.Lerp(StartRotation, 0, value) * 360);
         m_Transform.rotation = Quaternion.Euler(TempRotation);
+    }
+
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+        transform.parent = SelectedPieceParent;
+
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        base.OnPointerUp(eventData);
+        transform.parent = NormalParent;
     }
 }
