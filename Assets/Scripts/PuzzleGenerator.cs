b46de8b9c986c325c0c81b20f82bd5040a92f397
Nader Nour <nnmh1992@hotmail.com>
2017-02-24 17:33:10 +0300

    some edits in prepration for finishing developement

--- Assets/Scripts/PuzzleGenerator.cs
+++ Assets/Scripts/PuzzleGenerator.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleGenerator : MonoBehaviour {
+    public RectTransform[] RandomPositions;
+    public float RandomRadius;
 
+
+    PuzzlePiece[] PuzzlePieces;
 	// Use this for initialization
 	void Start () {
-		
+        PuzzlePieces = GetComponentsInChildren<PuzzlePiece>();
+		foreach(var Piece in PuzzlePieces)
+        {
+            Piece.SetRandomPositionAndRotation(RandomPositions[Random.Range(0, RandomPositions.Length)].position, RandomRadius);
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
--- Assets/Scripts/PuzzlePiece.cs
+++ Assets/Scripts/PuzzlePiece.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PuzzlePiece : MonoBehaviour {
+public class PuzzlePiece : EventTrigger {
+    private Vector3 EndPosition;
+    private float StartDistance;
+    private float StartRotation;
+    private RectTransform m_Transform;
+    // Use this for initialization
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        m_Transform = GetComponent<RectTransform>();
+        EndPosition = transform.position;
+    }
+
+    public void SetRandomPositionAndRotation(Vector2 InPos, float CircleRed)
+    {
+         m_Transform.position = InPos + Random.insideUnitCircle * CircleRed;
+        StartDistance = Vector3.Distance(EndPosition, m_Transform.position);
+        StartRotation = Random.value;
+        Vector3 TempRotation = new Vector3(0, 0, StartRotation * 360);
+        m_Transform.rotation = Quaternion.Euler(TempRotation);
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        m_Transform.position = Input.mousePosition;
+        float CurrentDistance = Vector3.Distance(EndPosition, m_Transform.position);
+        float value  = Mathf.InverseLerp(StartDistance, 0, CurrentDistance);
+        value = ;
+        Vector3 TempRotation = new Vector3(0, 0, Mathf.Lerp(StartRotation, 0, value) * 360);
+        m_Transform.rotation = Quaternion.Euler(TempRotation);
+    }
 }
